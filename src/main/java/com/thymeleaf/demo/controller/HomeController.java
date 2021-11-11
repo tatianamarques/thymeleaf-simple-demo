2d1d061d44430d50c558aaf4981193b7f0889465
Tatiana Marques <tatiana@multifacetaty.com.br>
2021-11-11 15:18:37 -0300

    Exercício básico de Thymeleaf

---
+++ src/main/java/com/thymeleaf/demo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.thymeleaf.demo.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/datetime")
+	public ModelAndView getCurrentDateAndTime() {
+		ModelAndView mav = new ModelAndView("home");
+		mav.addObject("currentDateAndTime", new Date());
+		return mav;
+	}
+}
